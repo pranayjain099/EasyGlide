f72996c182d90d5b435c05af1add0a38699c0270
pranayjain099 <pranayjain2001@gmail.com>
2024-03-20 22:47:49 +0530

    Folders structured

--- EasyGlide.php
+++ EasyGlide.php
@@ -1,17 +1,21 @@
 <?php
 /*
  * Plugin Name:       EasyGlide
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handles the slider
+ * Plugin URI:        https://wordpress.com/
+ * Description:       Slide-show
  * Version:           1.0
- * Requires at least: 5.2
+ * Requires at least: 5.6
  * Requires PHP:      7.2
  * Author:            Pranay Jain
- * Author URI:        https://author.example.com/
+ * Author URI:        https://pranayjain099.github.io/Mywebsite/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
- * Text Domain:       Easy-Glide
+ * Text Domain:       easy-glide
  * Domain Path:       /languages
  */
-?>
+
+if (!defined("ABSPATH")) {
+    die (' hehehehehe');
+    exit;
+}
