787e05435a8bb94aefd719ad1514725aa75f510d
pranayjain099 <pranayjain2001@gmail.com>
2024-03-20 21:25:21 +0530

    basic setup

---
+++ EasyGlide.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name:       EasyGlide
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handles the slider
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pranay Jain
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       Easy-Glide
+ * Domain Path:       /languages
+ */
+?>
