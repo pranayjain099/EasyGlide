1f1a4f1557c300db82975eb8f594f2db4c6c40f7
pranayjain099 <pranayjain2001@gmail.com>
2024-03-21 19:13:17 +0530

    registered activation and deactivation hook

--- EasyGlide.php
+++ EasyGlide.php
@@ -21,13 +21,36 @@ if (!defined("ABSPATH")) {
 
 if (!class_exists('Easy_Glide')) {
 
-    class Easy_Glde
+    class Easy_Glide
     {
         function __construct()
         {
             $this->define_constant();
         }
 
+        public static function activate()
+        {
+            /**
+             * Updating the option 'rewrite_rules' to an empty string during plugin activation is a common practice in WordPress plugin development. By updating the 'rewrite_rules' option to an empty string you're essentially triggering WordPress to rebuild the rewrite rules from scratch. This ensures that any custom rewrite rules added by your plugin are properly incorporated into the system.
+             * do not worry as soon as you will save a post these rules will be recovered again.
+             */
+            update_option('rewrite_rules', '');
+        }
+
+        public static function deactivate()
+        {
+            //  it flushes the rewrite rules.
+
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall()
+        {
+
+        }
+
+
+        // Defining constants 
         public function define_constant()
         {
             define('EASY_GLIDE_PATH', plugin_dir_path(__FILE__));
@@ -39,5 +62,9 @@ if (!class_exists('Easy_Glide')) {
 
 if (class_exists('Easy_Glide')) {
 
-    $easy_glide = new Easy_Glde();
+    // takes two parameter path and an array which contains class name and funciton name.
+    register_activation_hook(__FILE__, array('Easy_Glide', 'activate'));
+    register_deactivation_hook(__FILE__, array('Easy_Glide', 'deactivate'));
+    register_uninstall_hook(__FILE__, array('Easy_Glide', 'uninstall'));
+    $easy_glide = new Easy_Glide();
 }
