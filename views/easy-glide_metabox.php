565a958bc8db793e5f2b4743559a1560521f9949
pranayjain099 <pranayjain2001@gmail.com>
2024-03-28 00:24:32 +0530

    Displaying the data in the fields of metabox

--- views/easy-glide_metabox.php
+++ views/easy-glide_metabox.php
@@ -1,20 +1,32 @@
+<?php
+// Displaying the data in the fields of metabox
+
+$meta = get_post_meta($post->ID);
+
+// Retrieving the text 
+$link_text = get_post_meta($post->ID, 'easy_glide_link_text', true);
+
+// Retrieving the url
+$link_url = get_post_meta($post->ID, 'easy_glide_link_url', true);
+
+?>
 <table class="form-table mv-slider-metabox">
     <tr>
         <th>
-            <label for="mv_slider_link_text">Link Text</label>
+            <label for="easy_glide_link_text">Link Text</label>
         </th>
         <td>
-            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text"
-                value="" required>
+            <input type="text" name="easy_glide_link_text" id="easy_glide_link_text" class="regular-text link-text"
+                value="<?php echo (isset($link_text)) ? $link_text : ''; ?>" required>
         </td>
     </tr>
     <tr>
         <th>
-            <label for="mv_slider_link_url">Link URL</label>
+            <label for="easy_glide_link_url">Link URL</label>
         </th>
         <td>
-            <input type="url" name="mv_slider_link_url" id="mv_slider_link_url" class="regular-text link-url" value=""
-                required>
+            <input type="url" name="easy_glide_link_url" id="easy_glide_link_url" class="regular-text link-url"
+                value="<?php echo (isset($link_url)) ? $link_url : ''; ?>" required>
         </td>
     </tr>
 </table>
