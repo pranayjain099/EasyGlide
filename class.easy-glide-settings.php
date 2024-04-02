e3bc7b1c40921eb5e49894ac6081b0341a64f485
pranayjain099 <pranayjain2001@gmail.com>
2024-04-02 16:34:50 +0530

    Registered first section and field

--- class.easy-glide-settings.php
+++ class.easy-glide-settings.php
@@ -12,8 +12,42 @@ if (!class_exists('Easy_Glide_Settings')) {
             // $options will contains an array with all the values submitted by our form.
             self::$options = get_option('easy_glide_options');
 
+            // Register sections and fields 
+            add_action('admin_init', array($this, 'admin_init'));
+
         }
 
+
+        // Register sections and fields 
+        public function admin_init()
+        {
+            // Section 1
+            add_settings_section(
+                'easy_glide_main_section',
+                'How does it work?',
+                null,
+                'easy_glide_page1'
+            );
+
+
+            // Field 1.1
+            add_settings_field(
+                'easy_glide_shortcode',
+                'Shortcode',
+                array($this, 'easy_glide_shortcode_callback'),
+                'easy_glide_page1',
+                'easy_glide_main_section'
+            );
+        }
+
+        // Callback function of field
+        public function easy_glide_shortcode_callback()
+        {
+            ?>
+            <span>Use the shortcode [mv_slider] to display the slider in any page/post/widget</span>
+            <?php
+        }
     }
+
 }
 
